aa9e9d2f4ea635badb527c0e3fc7846f0fab685c
Kapea-D <221210055@nitdelhi.ac.in>
2024-04-12 14:47:44 +0530

    adding db

--- api/settings.py
+++ api/settings.py
@@ -80,8 +80,14 @@ WSGI_APPLICATION = 'api.wsgi.app'
 # DATABASES = {}
 
 
-DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'], engine='django_cockroachdb')}
+# DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'], engine='django_cockroachdb')}
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
